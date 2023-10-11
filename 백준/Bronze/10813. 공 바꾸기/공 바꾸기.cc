566b4437063001032cd69e02439985d1679fa66a
ParkGeonjo <80026010+ParkGeonjo@users.noreply.github.com>
2023-10-11 15:32:58 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[100], n, m, i, j, temp;
+    
+    cin >> n >> m;
+    
+    for(int x = 0; x < n; x++) a[x] = x + 1;
+    for(int x = 0; x < m; x++) {
+        cin >> i >> j;
+        temp = a[i - 1];
+        a[i - 1] = a[j - 1];
+        a[j - 1] = temp;
+    }
+    for(int x = 0; x < n; x++) cout << a[x] << " ";
+}
