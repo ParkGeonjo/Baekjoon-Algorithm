97d86b953cffb02c487a20e56ea01c84f8463418
ParkGeonjo <80026010+ParkGeonjo@users.noreply.github.com>
2023-10-12 14:59:54 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -1,30 +1,15 @@
 #include <iostream>
-
 using namespace std;
 
-int arr1[101][101], arr2[101][101];
-
 int main() {
-
-	int n, m;
-
-	cin >> n >> m;
-
-	for (int i = 0; i < n; i++) 
-		for (int k = 0; k < m; k++) 
-			cin >> arr1[i][k];
-
-
-	for (int i = 0; i < n; i++) 
-		for (int k = 0; k < m; k++) 
-			cin >> arr2[i][k];
-	
-
-	for (int i = 0; i < n; i++) {
-		for (int k = 0; k < m; k++) 
-			cout << arr1[i][k] + arr2[i][k] << ' ';
-		cout << '\n';
-	}
-
-	return 0;
+    int a[101][101], b[101][101], n, m;
+    
+    cin >> n >> m;
+    
+    for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) cin >> a[i][j];   
+    for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) cin >> b[i][j];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) cout << a[i][j] + b[i][j] << ' ';
+        cout << '\n';
+    }
 }
