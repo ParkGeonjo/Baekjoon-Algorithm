a6c778161c79b699fd789e3ec6c312f0dcef2a94
ParkGeonjo <80026010+ParkGeonjo@users.noreply.github.com>
2023-10-18 09:57:40 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
@@ -2,29 +2,16 @@
 using namespace std;
 
 int main() {
-    int t, c;
+    int t, c, coin[4] = {25, 10, 5, 1};
     
     cin >> t;
     
     for(int i = 0; i < t; i++) {
-        int Q = 0, D = 0, N = 0, P = 0;
         cin >> c;
-        while(c > 4) {
-            if(c >= 25) {
-                Q = c / 25;
-                c %= 25;
-            }
-            else if(c >= 10) {
-                D = c / 10;
-                c %= 10;
-            }
-            else {
-                N = c / 5;
-                c %= 5;
-            }
+        for(int j = 0; j < 4; j++) {
+            cout << c / coin[j] << " ";
+            c -= (c / coin[j]) * coin[j];
         }
-        P = c / 1;
-        
-        cout << Q << " " << D << " " << N << " " << P << endl;
-     }
+        cout << endl;
+    }
 }
