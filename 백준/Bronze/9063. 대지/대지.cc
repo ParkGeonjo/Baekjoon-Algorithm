895fbea8f9300ea607f91f165b536e3aebaef2ad
ParkGeonjo <80026010+ParkGeonjo@users.noreply.github.com>
2023-10-20 16:14:23 +0900

    [Bronze III] Title: 대지, Time: 44 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.cc
+++ 백준/Bronze/9063. 대지/대지.cc
@@ -6,14 +6,13 @@ int main() {
 
 	cin >> n;
 
-	for (int i = 0; i < n; i++) cin >> pos[i][0] >> pos[i][1];
-
 	for (int i = 0; i < n; i++) {
+        cin >> pos[i][0] >> pos[i][1];
 		x_min = x_min >= pos[i][0] ? pos[i][0] : x_min;
 		y_min = y_min >= pos[i][1] ? pos[i][1] : y_min;
 		x_max = x_max <= pos[i][0] ? pos[i][0] : x_max;
 		y_max = y_max <= pos[i][1] ? pos[i][1] : y_max;
-	}
+    }
 
 	cout << (x_max - x_min) * (y_max - y_min);
 }
