6020d70fcf5413162da3d9dc32c8f920b52d06e2
ParkGeonjo <80026010+ParkGeonjo@users.noreply.github.com>
2023-11-01 16:55:40 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, bag = 0;
+
+	cin >> n;
+
+	while (n > 0) {
+		if (n % 5 == 0) {
+			bag += n / 5;
+			n /= 5;
+			break;
+		}
+		n -= 3;
+		bag++;
+	}
+	if (n < 0) cout << -1;
+	else cout << bag;
+}
